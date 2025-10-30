618f806edba369b309e6f2f1e823d9a8b18d4621
kuchukmobeet <kuchuk.mobeet@gmail.com>
2025-10-30 21:30:16 +0530

    Initial push

---
+++ src/main/java/dev/kuku/authsome/AuthsomeCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.kuku.authsome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsomeCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthsomeCoreApplication.class, args);
+    }
+
+}
